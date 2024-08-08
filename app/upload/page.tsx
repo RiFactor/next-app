b6636118d11acc3f2410ff666dcec7cab73f7513
rk <khanri1@outlook.com>
2024-08-08 22:08:02 +0100

    customise upload widget

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -3,6 +3,7 @@
 import { CldImage, CldUploadWidget } from "next-cloudinary";
 import { useState } from "react";
 // https://next.cloudinary.dev/clduploadwidget/basic-usage
+// https://demo.cloudinary.com/uw/#/ customise widget
 
 type CloudinaryResult = {
   //mosh used interface
@@ -24,8 +25,19 @@ const page = () => {
         />
       )}
       <CldUploadWidget
+        options={{
+          sources: ["local"],
+          maxFileSize: 5,
+          multiple: false,
+          styles: {
+            palette: {
+              // sourceBg: "#E399C4",
+            },
+          },
+        }}
         uploadPreset="mosh-nextjs"
         onSuccess={(result, widget) => {
+          console.log(widget);
           if (result.event !== "success") return;
           const info = result.info as CloudinaryResult; // NB: b/c not typed
           setPublicId(info.public_id);
